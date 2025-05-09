64caefd1c4041843cb44e60eaf26fcc2c55c017b
Julie Simon <sim.ju@live.fr>
2025-05-09 23:39:40 +0200

    feat(logos et images) gestion des images avec dark mode

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module OtterlyWeb
     config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    config.assets.enable = true
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
